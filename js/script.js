11fcdac15ee2df3cdf0771327c72de7db57f613b
Ainur Dzhaianbaeva and Brittany Thirtyacre <brittany@>
2018-01-09 14:42:27 -0800

    working through bugs

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("p").click(function() {
+    $("img").show();
+  });
+});
+
+$(".my-class").show();
+$(".my-class").hide();
+$(".my-class").toggle();ß
+
+jQuery("h1").click(function() {
+  alert("This is a header.");
+});
+
+jQuery("p").click(function() {
+  alert("This is a paragraph.");
+});
+
+jQuery("img").click(function() {
+  alert("This is an image.");
+});
